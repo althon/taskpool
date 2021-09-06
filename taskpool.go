2abc3e1976ccc50c5f414ca7d9b3f8f9f2e4506b
althon <lovexue775@126.com>
2021-09-06 12:04:48 +0800

    first version

---
+++ taskpool.go
@@ -0,0 +1,54 @@
+package taskpool
+
+type Task struct {
+	F func(p ...interface{}) error
+	Param interface{}
+}
+
+func (t *Task) Do() error{
+	return t.F(t.Param)
+}
+
+type Pool struct {
+	tasks chan *Task
+	cap int
+}
+
+func NewTask(f func(params ...interface{}) error,p ...interface{}) *Task{
+	return &Task{f,p}
+}
+
+func NewPool(cap int) *Pool{
+	return &Pool{
+		make(chan *Task),
+		cap,
+	}
+}
+/*
+*/
+func (p *Pool) Enqueue(t *Task){
+	p.tasks <- t
+}
+
+func (p *Pool) Run(){
+	//根据内存池里数量启动协程
+	for i := 0; i < p.cap; i++{
+		go p.do(i+1)
+	}
+}
+
+func (p *Pool) Stop(){
+	close(p.tasks)
+}
+
+func (p *Pool) do(id int) {
+	//task不断的从dequeue队列中拿到任务
+	for  {
+		//如果拿到任务,则执行task任务
+		if task,ok := <- p.tasks;ok{
+			task.Do()
+		}else {
+			break
+		}
+	}
+}
